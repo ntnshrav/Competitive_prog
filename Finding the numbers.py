0410311b94583e1a964dfeef955fe59defdbdba4
Nitin Shravan <ntnshrav@gmail.com>
2020-03-24 00:18:51 +0530

    Create Finding the numbers.py

---
+++ Finding the numbers.py
@@ -0,0 +1,21 @@
+#https://practice.geeksforgeeks.org/problems/finding-the-numbers/0
+T = int(input())
+for t in range(T):
+    N = int(input())
+    arr = [int(x) for x in input().split()]
+    # print(arr)
+    
+    set_ =set()
+    
+    for i in range(len(arr)):
+        # print(arr[i])
+        if arr[i] in set_:
+            set_.remove(arr[i])
+            # pass
+        else:
+            set_.add(arr[i])
+            # pass
+    
+    arr_ = [x for x in arr if x in set_ ]
+    arr_ = sorted(set(arr_))
+    print(*arr_,sep=" ")
